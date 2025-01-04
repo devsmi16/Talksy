f7caf6c2f1e92ac6de40938b678813c26cad552f
Sami Gündoğan <gundogannsami@gmail.com>
2025-01-04 20:02:45 +0300

    Initial Commit

---
+++ Talksy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talksy
+//
+//  Created by Sami Gündoğan on 4.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
